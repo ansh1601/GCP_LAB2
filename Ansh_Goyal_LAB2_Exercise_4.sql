52aadcfda5f802985a3488badf055f583aedf2c6
ansh1601 <42743916+ansh1601@users.noreply.github.com>
2022-01-20 00:11:49 +0530

    Update Ansh_Goyal_LAB2_Exercise_4.sql

--- Ansh_Goyal_LAB2_Exercise_4.sql
+++ Ansh_Goyal_LAB2_Exercise_4.sql
@@ -1,5 +1,3 @@
-CREATE or REPLACE TABLE `nttdata-c4e-bde.uc1_18.myTable`
-AS
 with country_from as(
     SELECT country,ARRAY_AGG(STRUCT(
         operatingSystem ,browser,rank
@@ -9,10 +7,10 @@ with country_from as(
     device.browser as browser,
     ROW_NUMBER()  OVER (
         PARTITION By geoNetwork.country
-        ORDER BY (count(device.browser)+count(device.operatingSystem)) DESC
+        ORDER BY (count(device.browser)*count(device.operatingSystem)) DESC
     ) AS rank
     FROM `bigquery-public-data.google_analytics_sample.ga_sessions_20170801`
-    WHERE geoNetwork.country is NOT NULL 
+    WHERE device.isMobile = TRUE 
     group by country,operatingSystem,browser
 )
 WHERE country NOT like '(%)'
@@ -23,4 +21,32 @@ ORDER BY country
 
 
 
+/* IF you want to add Create Query with the table results then add 
+CREATE or REPLACE TABLE `dataset.tableID` at the beginning of your query.
 
+FOR Eg.
+
+CREATE OR REPLACE TABLE `uc1_18.myTable1`
+with country_from as(
+    SELECT country,ARRAY_AGG(STRUCT(
+        operatingSystem ,browser,rank
+        )LIMIT 3
+    ) as country_rank
+   FROM( SELECT geoNetwork.country as country,device.operatingSystem as operatingSystem,
+    device.browser as browser,
+    ROW_NUMBER()  OVER (
+        PARTITION By geoNetwork.country
+        ORDER BY (count(device.browser)*count(device.operatingSystem)) DESC
+    ) AS rank
+    FROM `bigquery-public-data.google_analytics_sample.ga_sessions_20170801`
+    WHERE device.isMobile = TRUE 
+    group by country,operatingSystem,browser
+)
+WHERE country NOT like '(%)'
+GROUP BY country
+)
+SELECT * FROM country_from
+ORDER BY country
+
+
+*/
