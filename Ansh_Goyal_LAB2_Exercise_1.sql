75d6bf8ba25b8ae5a7fe9eefb733ff863de35442
ansh1601 <42743916+ansh1601@users.noreply.github.com>
2022-01-19 13:53:15 +0530

    Update Ansh_Goyal_LAB2_Exercise_1.sql

--- Ansh_Goyal_LAB2_Exercise_1.sql
+++ Ansh_Goyal_LAB2_Exercise_1.sql
@@ -1,14 +1,13 @@
-with row_count as(
-select year,Movie_Title,Production_Budget,RANK() OVER(PARTITION BY year order by Production_Budget DESC) AS rank
-from
-(SELECT
-EXTRACT(YEAR FROM Release_Date) AS year,Movie_Title,Production_Budget
-FROM `nttdata-c4e-bde.uc1_13.Movie`
+with movie as(
+select EXTRACT(YEAR FROM Release_Date) AS year,
+Movie_Title,Production_Budget,
+RANK() OVER(PARTITION BY EXTRACT(YEAR FROM Release_Date)
+order by Production_Budget DESC) AS rank
+FROM `nttdata-c4e-bde.uc1_18.Movie`
 where Release_Date BETWEEN '2016-01-01' and '2020-12-31'
-)
 )
 SELECT
   year,Movie_Title,Production_Budget,rank
-FROM row_count
+FROM movie
 WHERE rank < 11
-ORDER BY year DESC,Production_Budget DESC
+ORDER BY year DESC
