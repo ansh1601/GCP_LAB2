9ed98e83bfa800fee311b82490d41a5d07a03106
ansh1601 <42743916+ansh1601@users.noreply.github.com>
2022-01-19 13:09:59 +0530

    Update Ansh_Goyal_LAB2_Exercise_7.sql

--- Ansh_Goyal_LAB2_Exercise_7.sql
+++ Ansh_Goyal_LAB2_Exercise_7.sql
@@ -2,4 +2,6 @@ SELECT Lan.name as language_name, Count(language) as count
 FROM `bigquery-public-data.github_repos.languages`
 CROSS JOIN UNNEST(language) Lan
 Group by language_name
-having language_name = "JavaScript"
+ORDER By count DESC
+LIMIT 1
+
