044ec48ec77bdb1825133d559df825aee21ef641
ansh1601 <42743916+ansh1601@users.noreply.github.com>
2022-01-20 13:43:51 +0530

    Update Ansh_Goyal_LAB2_Exercise_6.sql

--- Ansh_Goyal_LAB2_Exercise_6.sql
+++ Ansh_Goyal_LAB2_Exercise_6.sql
@@ -1,10 +1,13 @@
-SELECT DISTINCT(S.owner_user_id) AS id_user,
+SELECT S.owner_user_id AS id_user,
 COUNT(S.owner_user_id) as count
-FROM `bigquery-public-data.stackoverflow.stackoverflow_posts` S
-JOIN `bigquery-public-data.stackoverflow.posts_answers` P
+FROM `bigquery-public-data.stackoverflow.users` U
+INNER JOIN `bigquery-public-data.stackoverflow.posts_answers` P
+ON U.id=P.owner_user_id
+INNER JOIN `bigquery-public-data.stackoverflow.stackoverflow_posts` S
 ON S.owner_user_id=P.owner_user_id
-WHERE S.accepted_answer_id IS NOT NULL
-GROUP BY id_user,S.creation_date
-HAVING EXTRACT (YEAR FROM S.creation_date)=2010
+WHERE S.accepted_answer_id IS NOT NULL and 
+EXTRACT (YEAR FROM S.creation_date)=2010
+GROUP BY id_user
 ORDER BY count DESC 
 LIMIT 10
+
