ee4b9a500ddccb6965012dd93443b8ddd1ee0a01
ansh1601 <42743916+ansh1601@users.noreply.github.com>
2022-01-20 16:11:41 +0530

    Update Ansh_Goyal_LAB2_Exercise_6.sql

--- Ansh_Goyal_LAB2_Exercise_6.sql
+++ Ansh_Goyal_LAB2_Exercise_6.sql
@@ -1,13 +1,12 @@
-SELECT S.owner_user_id AS id_user,
-COUNT(S.owner_user_id) as count
+SELECT U.id AS id_user,
+COUNT(P.owner_user_id) as count
 FROM `bigquery-public-data.stackoverflow.users` U
 INNER JOIN `bigquery-public-data.stackoverflow.posts_answers` P
 ON U.id=P.owner_user_id
 INNER JOIN `bigquery-public-data.stackoverflow.stackoverflow_posts` S
-ON S.owner_user_id=P.owner_user_id
+ON S.accepted_answer_id=P.id
 WHERE S.accepted_answer_id IS NOT NULL and 
-EXTRACT (YEAR FROM S.creation_date)=2010
+EXTRACT (YEAR FROM P.creation_date)=2010 
 GROUP BY id_user
-ORDER BY count DESC 
+ORDER BY count DESC
 LIMIT 10
-
