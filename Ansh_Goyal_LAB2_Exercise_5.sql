1dc63d00890c8fbf2de4ac61fa1f7cdec04cc01d
ansh1601 <42743916+ansh1601@users.noreply.github.com>
2022-01-20 16:02:21 +0530

    Update Ansh_Goyal_LAB2_Exercise_5.sql

--- Ansh_Goyal_LAB2_Exercise_5.sql
+++ Ansh_Goyal_LAB2_Exercise_5.sql
@@ -4,6 +4,7 @@ FROM `bigquery-public-data.stackoverflow.users` U
 JOIN
 `bigquery-public-data.stackoverflow.posts_answers` P
 ON U.id=P.owner_user_id
+WHERE EXTRACT (YEAR from P.creation_date)=2010
 group by id_user
 ORDER BY count DESC
 LIMIT 10
